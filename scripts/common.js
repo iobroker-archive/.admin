2f112e7a14169ae5786ca726ee5a669d2f50c34f
Martin M <mcm57@gmx.at>
2024-01-12 17:09:47 +0100

    Initial release

---
+++ scripts/common.js
@@ -0,0 +1,255 @@
+const axios = require('axios');
+
+// LABEL support
+
+//function addLabel(prID, label) {
+//    return axios.post(`https://api.github.com/repos/ioBroker/ioBroker.repositories/issues/${prID}/labels`,
+//        {
+//            [ label ]
+//        },
+//        {
+//            headers: {
+//                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+//                'user-agent': 'Action script'
+//            }
+//        })
+//        .then(response => response.data);
+//}
+
+function addLabels(prID, labels) {
+    return axios.post(`https://api.github.com/repos/ioBroker/ioBroker.repositories/issues/${prID}/labels`,
+        {
+            labels
+        },
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            }
+        })
+        .then(response => response.data);
+}
+
+function deleteLabel(prID, label) {
+    let url = `labels/${label}`;
+    if (prID) {
+        url= `issues/${prID}/labels/${label}`
+    }
+    return axios.delete(`https://api.github.com/repos/ioBroker/ioBroker.repositories/${url}`, {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        }
+    })
+    .then(response => response.data);
+}
+
+function getLabels(prID) {
+    let url = `labels`;
+    if (prID) {
+        url= `issues/${prID}/labels`
+    }
+    return axios(`https://api.github.com/repos/ioBroker/ioBroker.repositories/${url}`,
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            }
+        })
+        .then(response => response.data )
+}
+
+function createLabel(name, description, color) {
+        return axios.post(`https://api.github.com/repos/ioBroker/ioBroker.repositories/labels`,
+        {
+            'name': `${name}`,
+            'description': `${description}`,
+            'color': `${color}`
+        },
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            }
+        })
+        .then(response => response.data);
+}
+
+function updateLabel(name, description, color) {
+    return axios.patch(`https://api.github.com/repos/ioBroker/ioBroker.repositories/labels/${name}`,
+    {
+        'description': `${description}`,
+        'color': `${color}`
+    },
+    {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        }
+    })
+    .then(response => response.data);
+}
+
+function addComment(prID, body) {
+    return axios.post(`https://api.github.com/repos/ioBroker/ioBroker.repositories/issues/${prID}/comments`, {body},
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            },
+        })
+        .then(response => response.data);
+}
+
+// COMMENT handling
+
+function getAllComments(prID) {
+    ///repos/:owner/:repo/issues/:issue_number/comments
+    return axios(`https://api.github.com/repos/ioBroker/ioBroker.repositories/issues/${prID}/comments?per_page=100`, {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        }
+    })
+        .then(response => response.data);
+}
+
+function deleteComment(prID, commentID) {
+///repos/:owner/:repo/issues/:issue_number/comments
+    return axios.delete(`https://api.github.com/repos/ioBroker/ioBroker.repositories/issues/comments/${commentID}`, {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        }
+    })
+        .then(response => response.data);
+}
+
+// ISSUE handling
+
+function createIssue(owner, adapter, json) {
+    /*
+    {
+      "title": "Found a bug",
+      "body": "I'm having a problem with this.",
+      "assignees": [
+        "octocat"
+      ],
+      "milestone": 1,
+      "labels": [
+        "bug"
+      ]
+    }
+*/
+    return axios.post(`https://api.github.com/repos/${owner}/${adapter}/issues`, json, {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        },
+    })
+        .then(response => response.data);
+}
+
+// REPOSITORY handling
+
+async function isRepository (owner, repository) {
+    try {
+        const result = await getGithub( `https://api.github.com/repos/${owner}/${repository}` );
+//console.log( `https://api.github.com/repos/${owner}/${repository}`);
+//console.log (result);
+        return result.id;
+    } catch {
+        return false;
+    }
+}
+
+function forkRepository (srcOwner, srcRepo, destOrga, destRepo) {
+    return axios.post(`https://api.github.com/repos/${srcOwner}/${srcRepo}/forks`,
+        {
+            'organization': destOrga,
+            'name': destRepo,
+            'default_branch_only': true
+        },
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            }
+        })
+        .then(response => response.data)
+        .catch(e => {
+            console.error(`[forkRepository] ${JSON.stringify(e)}`)
+        });
+}
+
+async function syncRepository (owner, repository) {
+    const result = await getGithub( `https://api.github.com/repos/${owner}/${repository}`);
+//console.log( `https://api.github.com/repos/${owner}/${repository}`);
+//console.log (result.default_branch);
+    const branch = result.default_branch;
+    console.log( `debug: use branch ${branch}`);
+    return axios.post(`https://api.github.com/repos/${owner}/${repository}/merge-upstream`,
+        {
+            'branch': branch
+        },
+        {
+            headers: {
+                Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+                'user-agent': 'Action script'
+            }
+        })
+        .then(response => response.data)
+        .catch(e => {
+            console.error(`${JSON.stringify(e)}`)
+        });
+}
+
+// general purpose
+
+function getGithub(url, raw) {
+    const options = {
+        headers: {
+            Authorization: process.env.OWN_GITHUB_TOKEN ? `token ${process.env.OWN_GITHUB_TOKEN}` : 'none',
+            'user-agent': 'Action script'
+        },
+    };
+    if (!process.env.OWN_GITHUB_TOKEN) {
+        delete options.headers.Authorization;
+    }
+    if (raw) {
+        options.transformResponse = [];
+    }
+
+    return axios(url, options)
+        .then(response => response.data)
+        .catch(e => {
+            console.error(`Cannot read ${url}: ${e}`);
+            throw e;
+        });
+}
+
+function getUrl(url, asText) {
+    console.log(`Read ${url}`);
+    return axios(url, asText ? {transformResponse: x => x} : {})
+        .then(response => response.data);
+}
+
+module.exports = {
+//    addLabel,
+    addLabels,
+    createLabel,
+    deleteLabel,
+    updateLabel,
+
+    getGithub,
+    getUrl,
+    createIssue,
+    addComment,
+    deleteComment,
+    getAllComments,
+    getLabels,
+
+    forkRepository,
+    isRepository,
+    syncRepository,
+};
---
+++ scripts/updateForks.js
@@ -0,0 +1,108 @@
+'use strict';
+const {
+    addLabel,
+    createLabel,
+    deleteLabel,
+    updateLabel,
+    getLabels,
+    getAllComments,
+    getGithub,
+    getUrl,
+
+    forkRepository,
+    isRepository,
+    syncRepository,
+} = require('./common');
+
+function getLatestRepo() {
+    return getUrl('http://repo.iobroker.live/sources-dist-latest.json');
+}
+
+function getStableRepo() {
+    return getUrl('http://repo.iobroker.live/sources-dist.json');
+}
+
+async function mergeRepos(latest, stable) {
+    const adapters = [];
+
+    console.log('');
+    console.log('reading STABLE repository');
+    for (const adapter in stable) {
+        if (adapter.startsWith('_')) {
+            console.log(`SKIPPING ${adapter}`);
+        } else {
+            const parts = stable[adapter].meta.split('/');
+            const owner = parts[3];
+            const item = {
+                adapter,
+                owner
+            };
+            console.log(`adding ${adapter}`);
+            adapters.push(item);
+        }
+    }
+
+    console.log('reading LATEST repository');
+    for (const adapter in latest) {
+        if (adapter.startsWith('_')) {
+            console.log(`SKIPPING ${adapter}`);
+        } else {
+            const parts = latest[adapter].meta.split('/');
+            const owner = parts[3];
+            if (!adapters.find(e => e.adapter === adapter)) {
+                const item = {
+                    adapter,
+                    owner
+                };
+                console.log(`adding ${adapter}`);
+                adapters.push(item);
+            }
+        }
+    }
+    return adapters;
+}
+
+
+async function forkAndSyncRepo( pOwner, pRepository ){
+    const repoName = `${pRepository}.-.${pOwner}`;
+    if ( await isRepository( 'iobroker-archive', repoName)) {
+        console.log (`syncing repository iobroker-archive/${repoName} from ${pOwner}/${pRepository}`);
+        const result = await syncRepository( 'iobroker-archive', repoName);
+        console.log( result );
+    } else {
+        console.log (`forking repository ${pOwner}/${pRepository} as iobroker-archive/${repoName}`);
+        const result = await forkRepository( pOwner, pRepository, 'iobroker-archive', repoName);    
+        console.log( result.id ? `forking OK (id: ${result.id})` : 'forking failed' );
+    }
+
+
+}
+
+async function doIt() {
+    // get repos
+    const stable = await getStableRepo();
+    const latest = await getLatestRepo();
+    const adapters = await mergeRepos (latest, stable);
+
+    for (const adapter of adapters) {
+        console.log('');
+        console.log(`processing ${adapter.adapter}...`);
+        await forkAndSyncRepo( adapter.owner, `ioBroker.${adapter.adapter}`);
+//        return 'stopped';
+    }
+
+    return 'done';
+}
+
+// activate for debugging purposes
+// process.env.GITHUB_REF = 'refs/pull/2348/merge';
+// process.env.OWN_GITHUB_TOKEN = 'insert here';
+// process.env.GITHUB_EVENT_PATH = __dirname + '/../event.json';
+
+//console.log(`process.env.GITHUB_REF        = ${process.env.GITHUB_REF}`);
+//console.log(`process.env.GITHUB_EVENT_PATH = ${process.env.GITHUB_EVENT_PATH}`);
+console.log(`process.env.OWN_GITHUB_TOKEN  = ${(process.env.OWN_GITHUB_TOKEN || '').length}`);
+
+doIt()
+    .then(result => console.log(result))
+    .catch(e => console.error(e));
