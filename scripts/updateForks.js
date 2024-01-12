a75a9f18c1b18724b6ba6c8138438d09bd2bbd1b
Martin M <mcm57@gmx.at>
2024-01-12 20:16:55 +0100

    Update updateForks.js

--- scripts/updateForks.js
+++ scripts/updateForks.js
@@ -132,6 +132,9 @@ async function doIt() {
     for (const name of repoFailed) {
         console.log(`    ${name}`);
     }
+     if (repoFailed.length) {
+        RadioNodeList( 'error: some updates failed')
+     }
     return 'done';
 }
 
@@ -146,4 +149,4 @@ console.log(`process.env.OWN_GITHUB_TOKEN  = ${(process.env.OWN_GITHUB_TOKEN || 
 
 doIt()
     .then(result => console.log(result))
-    .catch(e => console.error(e));
+    .catch(e => { console.error(e); reraise(e) });
